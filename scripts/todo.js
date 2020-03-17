bc778a1e8e0e0bf709478d98d24109804203056f
YYSA10 <pya310@gmail.com>
2020-03-17 15:15:38 +0900

    リストが空の時のコメントを追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,17 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('リストは空です')
+		}
 		msg.send(todo.list().join('\n'));
 	});
 	robot.respond(/donelist/i, (msg) => {
+		const list = todo.list();
+		if (list.length === 0) {
+			msg.send('リストは空です');
+		};
 		msg.send(todo.donelist().join('\n'));
 	});
 };
